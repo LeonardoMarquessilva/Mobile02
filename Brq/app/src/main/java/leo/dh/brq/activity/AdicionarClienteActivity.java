087af91e6f2b76124b2b1444dd925708904b6ca0
LeonardoMarquesSilva <leonardomarques84@hotmail.com>
2018-12-16 05:13:43 -0200

    Adicionado consumo de API via cep

--- Brq/app/src/main/java/leo/dh/brq/activity/AdicionarClienteActivity.java
+++ Brq/app/src/main/java/leo/dh/brq/activity/AdicionarClienteActivity.java
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import leo.dh.brq.R;
+import leo.dh.brq.buscar_endereco.Address;
+import leo.dh.brq.buscar_endereco.Util;
+import leo.dh.brq.buscar_endereco.ZipCodeListener;
 import leo.dh.brq.helper.ClienteDAO;
 import leo.dh.brq.model.Cliente;
 
@@ -16,12 +22,17 @@ public class AdicionarClienteActivity extends AppCompatActivity {
     private TextInputEditText editCliente;
     private Cliente clienteAtual;
 
+    private EditText etZipCode;
+    private Util util;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_cliente);
 
         editCliente = findViewById(R.id.textCliente);
+        etZipCode = (EditText) findViewById(R.id.et_zip_code);
+        etZipCode.addTextChangedListener( new ZipCodeListener( this ) );
 
         // Recuperar cliente, caso seja edicao
         clienteAtual = ( Cliente ) getIntent().getSerializableExtra("clienteSelecionado");
@@ -32,8 +43,58 @@ public class AdicionarClienteActivity extends AppCompatActivity {
 
         }
 
+        //Spinner spStates = (Spinner) findViewById(R.id.sp_state);
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter
+          //      .createFromResource(this,
+            //            R.array.states,
+              //          android.R.layout.simple_spinner_item);
+        //spStates.setAdapter(adapter);
+
+        util = new Util(this,
+                R.id.et_zip_code,
+                R.id.et_street,
+                R.id.et_complement,
+                R.id.et_neighbor,
+                R.id.et_city,
+                R.id.sp_state,
+                R.id.et_number);
+                //R.id.tv_zip_code_search
+
     }
 
+    public String getUriZipCode(){
+        return "https://viacep.com.br/ws/"+etZipCode.getText()+"/json/";
+    }
+
+    public void lockFields( boolean isToLock ){
+        util.lockFields( isToLock );
+    }
+
+    public void setDataViews(Address address){
+        setField( R.id.et_street, address.getLogradouro() );
+        setField( R.id.et_complement, address.getComplemento() );
+        setField( R.id.et_neighbor, address.getBairro() );
+        setField( R.id.et_city, address.getLocalidade() );
+        setField( R.id.sp_state, address.getUf() );
+    }
+
+    private void setField( int id, String data ){
+        ((EditText) findViewById(id)).setText( data );
+    }
+
+   /* private void setSpinner( int id, int arrayId, String data ){
+        String[] itens = getResources().getStringArray(arrayId);
+
+        for( int i = 0; i < itens.length; i++ ){
+
+            if( itens[i].endsWith( "("+data+")" ) ){
+                ((Spinner) findViewById(id)).setSelection( i );
+                return;
+            }
+        }
+        ((Spinner) findViewById(id)).setSelection( 0 );
+    }
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_adicionar_cliente, menu);
